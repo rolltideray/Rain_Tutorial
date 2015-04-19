13da2603e0d1ae049867d6fbd6b28f04101be6a8
Ray Young <rolltideray@gmail.com>
2015-04-19 15:57:51 -0500

    Episode 5

--- Rain/src/com/bamadroid/rain/Game.java
+++ Rain/src/com/bamadroid/rain/Game.java
@@ -2,6 +2,7 @@ package com.bamadroid.rain;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -40,9 +41,23 @@ public class Game extends Canvas implements Runnable {
 			
 	public void run(){
 		while(running){
-			System.out.println("Running....");
+			update();
+			render();
 		}
 	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(){
+		BufferStrategy bs  getBufferStrategy();
+		if (bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+	}
+	
 	
 	public static void main(String[] args){
 		Game game = new Game();
