032a869ef7b6b0f957573134b97a1a7339bb9fe7
Ray Young <rolltideray@gmail.com>
2015-04-19 17:31:42 -0500

    Episode 13

--- Rain/src/com/bamadroid/rain/Game.java
+++ Rain/src/com/bamadroid/rain/Game.java
@@ -52,10 +52,20 @@ public class Game extends Canvas implements Runnable {
 	}
 			
 	public void run(){
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
 		while(running){
-			update();
+			long now = System.nanoTime();
+			delta += (now-lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1){
+				update();
+				delta--;
+			}
 			render();
 		}
+		stop();
 	}
 	
 	public void update(){
