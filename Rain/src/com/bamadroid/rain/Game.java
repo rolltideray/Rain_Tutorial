502bced9cf04fdae298ee26ebd1c1eee77b76161
Ray Young <rolltideray@gmail.com>
2015-04-19 15:59:34 -0500

    Fixed error line 54 in Episode 5

--- Rain/src/com/bamadroid/rain/Game.java
+++ Rain/src/com/bamadroid/rain/Game.java
@@ -51,7 +51,7 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void render(){
-		BufferStrategy bs  getBufferStrategy();
+		BufferStrategy bs =  getBufferStrategy();
 		if (bs == null){
 			createBufferStrategy(3);
 			return;
