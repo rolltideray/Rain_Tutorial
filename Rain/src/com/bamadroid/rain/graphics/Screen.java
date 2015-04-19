1d30114cc3b60ee29f9da7a0683034214752d81e
Ray Young <rolltideray@gmail.com>
2015-04-19 16:35:08 -0500

    Episode 8

---
+++ Rain/src/com/bamadroid/rain/graphics/Screen.java
@@ -0,0 +1,13 @@
+package com.bamadroid.rain.graphics;
+
+public class Screen {
+	
+	private int width, height; 
+	public int[] pixels;
+	
+	public Screen(int width, int height){
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+}
