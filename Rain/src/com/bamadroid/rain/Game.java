58b5d86bf36e44bc4fa557c6a68210364f2a540d
Ray Young <rolltideray@gmail.com>
2015-04-19 20:00:14 -0500

    Episode 17

--- Rain/src/com/bamadroid/rain/Game.java
+++ Rain/src/com/bamadroid/rain/Game.java
@@ -11,6 +11,7 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 import com.bamadroid.rain.graphics.Screen;
+import com.bamadroid.rain.input.Keyboard;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
@@ -23,6 +24,7 @@ public class Game extends Canvas implements Runnable {
 	
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 	private boolean running = false;
 	
 	private Screen screen;
@@ -36,6 +38,9 @@ public class Game extends Canvas implements Runnable {
 		
 		screen = new Screen(width, height);
 		frame = new JFrame();
+		key = new Keyboard();
+		
+		frame.addKeyListener(key);
 	}
 	
 	public synchronized void start(){
@@ -83,8 +88,11 @@ public class Game extends Canvas implements Runnable {
 		stop();
 	}
 	
+	int x = 0, y = 0; 
 	public void update(){
-		
+		key.update();
+		x++;
+		//y++;
 	}
 	
 	public void render(){
@@ -94,8 +102,7 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		screen.clear();
-		
-		screen.render();
+		screen.render(x,y);
 		
 		for(int i = 0; i < pixels.length; i++){
 			pixels[i] = screen.pixels[i];
