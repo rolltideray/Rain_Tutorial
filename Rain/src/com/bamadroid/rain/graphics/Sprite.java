e7317f63ce471b2850043d9e4d6d08fdab6e2e2b
Ray Young <rolltideray@gmail.com>
2015-04-19 21:32:11 -0500

    Episode 21

--- Rain/src/com/bamadroid/rain/graphics/Sprite.java
+++ Rain/src/com/bamadroid/rain/graphics/Sprite.java
@@ -5,9 +5,10 @@ public class Sprite {
 	private final int SIZE;
 	private int x, y;
 	public int[] pixels;
-	
 	private SpriteSheet sheet;
 	
+	public static Sprite grass = new Sprite(16, 0, 0, null);
+	 
 	public Sprite(int size, int x, int y, SpriteSheet sheet){
 		SIZE = size;
 		pixels = new int[SIZE * SIZE];
