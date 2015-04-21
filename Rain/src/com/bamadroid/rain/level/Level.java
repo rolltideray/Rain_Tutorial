26e6ff960d9b0a3662da4f3f8cc77a8913043d1f
Ray Young <rolltideray@gmail.com>
2015-04-20 21:39:02 -0500

    Episode 25

--- Rain/src/com/bamadroid/rain/level/Level.java
+++ Rain/src/com/bamadroid/rain/level/Level.java
@@ -4,8 +4,8 @@ import com.bamadroid.rain.graphics.Screen;
 
 public class Level {
 	
-	private int width, height;
-	private int[] tiles;
+	protected int width, height;
+	protected int[] tiles;
 	
 	public Level(int width, int height){
 		
@@ -19,7 +19,7 @@ public class Level {
 		loadLevel(path);
 	}
 
-	private void generateLevel() {
+	protected void generateLevel() {
 		
 		
 	}
