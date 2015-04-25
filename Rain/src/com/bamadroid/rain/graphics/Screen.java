0a8810634a2e10aceb86ba8feaea0c700e55206f
Ray Young <rolltideray@gmail.com>
2015-04-24 23:13:43 -0500

    Episode 37

--- Rain/src/com/bamadroid/rain/graphics/Screen.java
+++ Rain/src/com/bamadroid/rain/graphics/Screen.java
@@ -6,11 +6,14 @@ import com.bamadroid.rain.level.tile.Tile;
 
 public class Screen {
 	
-	private int width, height; 
+	public int width, height; 
 	public int[] pixels;
 	public final int MAP_SIZE = 64;
+	public int xOffset, yOffset;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
+	
+	
 	
 	private Random random = new Random();
 	
@@ -32,28 +35,28 @@ public class Screen {
 		}
 	}
 	
-	public void render(int xOffset, int yOffset){
-		for(int y = 0; y < height; y++){
-			int yp = y + yOffset;
-			if(yp < 0 || yp >= height) continue;
-			for (int x = 0; x < width; x++){
-				int xp = x + xOffset;
-				if(xp < 0 || xp >= width) continue;
-				pixels[xp + yp * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
-			}
-		}
-	}
 	
 	public void renderTile(int xp, int yp, Tile tile){
-		for (int y=0;y< tile.sprite.SIZE;y++){
+		xp -= xOffset;
+		yp -= yOffset;
+		for (int y = 0; y < tile.sprite.SIZE; y++){
 			int ya = y + yp;
-			for (int x=0;x< tile.sprite.SIZE;x++){
+			for (int x = 0; x < tile.sprite.SIZE; x++){
 				int xa = x + xp;
-				if(xa < 0 || xa >= width || ya < 0 || ya >= width)break; // keep you from rendering what you can see on your screen
-				pixels[xa+ya*width] = tile.sprite.pixels[x + y  * tile.sprite.SIZE];
+				//if(xa < 0 || xa >= width || ya < 0 || ya >= width)break; // keep you from rendering what you can see on your screen
+				if (xa < -tile.sprite.SIZE || xa >= this.width || ya < 0 || ya >= this.height) break;
+				if(xa < 0){
+					xa = 0;
+				}
+				pixels[xa + ya * width] = tile.sprite.pixels[x + y  * tile.sprite.SIZE];
+				
 			}
 		}
 	}
 	
+	public void setOffset(int xOffset, int yOffset){
+		this.xOffset = xOffset;
+		this. yOffset = yOffset;
+	}
 	
 }
