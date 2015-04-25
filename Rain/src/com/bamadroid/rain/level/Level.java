9f5a35a1e9af3cc8dd819835ab4a3bc3925a8c72
Ray Young <rolltideray@gmail.com>
2015-04-24 23:30:50 -0500

    Episode 38 -- Back on track!

--- Rain/src/com/bamadroid/rain/level/Level.java
+++ Rain/src/com/bamadroid/rain/level/Level.java
@@ -41,14 +41,21 @@ public class Level {
 	public void render(int xScroll, int yScroll, Screen screen){
 		screen.setOffset(xScroll, yScroll);
 		int x0 = xScroll >> 4;
-		int x1 = (xScroll  + screen.width) >> 4;
+		int x1 = ((xScroll  + screen.width) >> 4) + 16;
 		int y0 = yScroll >> 4;
-		int y1 = (yScroll  + screen.height) >> 4;
-		getTile(x0, y0);
+		int y1 = ((yScroll  + screen.height) >> 4) + 16;
+		
+		for (int y = y0; y < y1; y++){
+			for (int x = x0; x < x1; x++){
+				getTile(x, y).render(x, y, screen);
+			}
+		}
+		
 	}
 	
 	public Tile getTile(int x, int y){
-		if (tiles[x+y*width] == 0) return Tile.grass;
+		if (x < 0 ||x >= width || y < 0 || y >= height) return Tile.voidTile;
+		if (tiles[x + y * width] == 0) return Tile.grass;
 		return Tile.voidTile;
 	}
 	
