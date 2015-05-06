fc691e50e3497f2112ecef9377c10b3c29bc44cf
Ray Young <rolltideray@gmail.com>
2015-05-06 14:08:25 -0500

    Episode 53

--- Rain/src/com/bamadroid/rain/level/SpawnLevel.java
+++ Rain/src/com/bamadroid/rain/level/SpawnLevel.java
@@ -21,6 +21,7 @@ public class SpawnLevel extends Level {
 			BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = image.getWidth();
 			int h = image.getHeight();
+			tiles = new Tile[w*h];
 			image.getRGB(0, 0, w, h, levelPixels, 0, w);
 		} catch (IOException e){
 			e.printStackTrace();
@@ -28,7 +29,16 @@ public class SpawnLevel extends Level {
 		}
 	}
 	
+	// Grass = 0xFF00
+	// Flower = 0xFFFF00
+	// Rock = 0x7F7F00
 	protected void generateLevel(){
+		for (int i = 0; i < levelPixels.length; i++){
+			if (levelPixels[i] == 0xFF00) tiles[i] = Tile.grass;
+			if (levelPixels[i] == 0xFFFF00) tiles[i] = Tile.flower;
+			if (levelPixels[i] == 0x7F7F00) tiles[i] = Tile.rock;
+			}
+		}
 		
 	}
 
